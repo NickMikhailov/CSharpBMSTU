56e14d2109947b28be859e49218efe47b7be52b6
NickMikhailov <124362971+NickMikhailov@users.noreply.github.com>
2023-03-27 12:04:30 +0300

    ДЗ №1

---
+++ AssignmentsCSharp/Library.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TeachMeHowToPrint
+{
+    internal static class Library
+    {
+        
+        static string textToType;
+        static String? PATH = "..\\..\\..\\Library\\";
+        public static FileInfo[]? getTextList () {
+            if (PATH == null) return null;
+            DirectoryInfo library = new DirectoryInfo(PATH);
+            return library.GetFiles();
+        }
+
+        public static string printRandomText()
+        {
+            Random random = new Random();
+            var textList = getTextList();
+            if (textList == null || textList.Length == 0)
+            {
+                throw new Exception("Библиотека не найдена!");
+            }
+
+            var textNumber = random.Next(0, textList.Length);
+            string path = textList[textNumber].FullName;
+
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] b = new byte[2048];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                int readLen;
+                while ((readLen = fs.Read(b, 0, b.Length)) > 0)
+                {
+                    textToType = temp.GetString(b, 0, readLen);
+                }
+                fs.Close();
+            }
+
+            return textToType;
+            
+        }
+
+        public static string printText()
+            {
+                textToType = "C# (произносится си шарп) " +
+                "— объектно-ориентированный язык программирования общего назначения. " +
+                "Разработан в 1998—2001 годах группой инженеров компании Microsoft " +
+                "под руководством Андерса Хейлсберга и Скотта Вильтаумота" +
+                " как язык разработки приложений для платформы Microsoft .NET Framework" +
+                " и .NET Core. Впоследствии был стандартизирован как ECMA-334 и ISO/IEC 23270." +
+                "\nC# относится к семье языков с C-подобным синтаксисом, " +
+                "из них его синтаксис наиболее близок к C++ и Java. " +
+                "Язык имеет статическую типизацию, поддерживает полиморфизм, перегрузку операторов " +
+                "(в том числе операторов явного и неявного приведения типа), " +
+                "делегаты, атрибуты, события, переменные, свойства, обобщённые типы и методы, " +
+                "итераторы, анонимные функции с поддержкой замыканий, LINQ, " +
+                "исключения, комментарии в формате XML."
+;
+                return textToType;
+            }
+
+        }
+}
+
--- AssignmentsCSharp/Program.cs
+++ AssignmentsCSharp/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using _01_TeachMeHowToPrint;
+
+Console.WriteLine("Hello! This application should learn you how to type!\nPress Enter to start or any other key to Exit\n");
+
+while (Console.ReadKey().KeyChar == 13)
+{
+//  Console.WriteLine("\n" + Library.printText() + "\nJust start to type\n(press Enter to finish)"); // версия с одним текстом
+    Console.WriteLine("\n" + Library.printRandomText() + "\nJust start to type\n(press Enter to finish)\n"); //версия с разными текстами
+    Console.ReadKey();
+    DateTime startedAt = DateTime.Now;
+    string? textTyped = Console.ReadLine();
+    TimeSpan span = DateTime.Now - startedAt;
+
+    if (textTyped == null || textTyped == "")
+    {
+        Console.WriteLine("\nYour typed nothing!\n");
+    }
+    else
+    {
+        Console.WriteLine("\nYour time is " + span + " sec");
+        double speed = Math.Round(textTyped.Length / (span.TotalMinutes));
+        Console.WriteLine("Your typing speed is " + speed + " symbols per min");
+    }
+    Console.WriteLine("\nTry again? \n(press Enter to Restart or any other key to Exit)");
+    }
